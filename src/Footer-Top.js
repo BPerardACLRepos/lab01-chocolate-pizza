3f8cf10295571be74b1b57c0a8a1e04685dd1b9e
Brandon Perard <brandon.perard@gmail>
2021-02-08 16:48:20 -0800

    update Foot-Top.js content

---
+++ src/Footer-Top.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class FooterTop extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={van - pic.png} alt='Vanessa Stevenson' />
+                <div>
+                    <h1>Vanessa Stevenson</h1>
+                    <p>Food enthusiast, photography fan. Add a pinch of raw foodism and that's pretty much who I am.</p>
+                </div>
+                <button>SHARE RECIPE</button>
+            </div>
+        );
+    }
+}
