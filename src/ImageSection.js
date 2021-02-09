f13e586c083d33b08dde7c8dd6abe56f77623653
Parker Sequeira <phsequeira@yahoo.com>
2021-02-08 16:14:27 -0800

    added print icon to imageSection

--- src/ImageSection.js
+++ src/ImageSection.js
@@ -7,6 +7,8 @@ class Title extends React.Component {
             <div>   
                 <h1>Chocolate Pizza</h1>
                 <p>POSTED ON 15 DEC 2013 / DESSERTS</p>
+                <img src = 'print-icon.png'/>
+                <p>PRINT</p>
             </div>
         )
     }
