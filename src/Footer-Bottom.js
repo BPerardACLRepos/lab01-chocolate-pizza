ed7f31eab9def970866d880d83daa7a92f808288
Brandon Perard <brandon.perard@gmail>
2021-02-08 16:54:52 -0800

    update Footer-Bottom.js content

---
+++ src/Footer-Bottom.js
@@ -0,0 +1,14 @@
+import { logDOM } from '@testing-library/react';
+import React from 'react';
+
+export default class FooterBottom extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={small - logo.png} alt='small logo' />
+                <p>Delicious &copy; 2013 &#8226; All Rights Reserved.</p>
+                <p>Proudly published with Ghost.</p>
+            </div>
+        );
+    }
+}
